ca62d088ce460b98ab94a4d40440732120ee704a
AlanKelly-STUDENT <g00484675@atu.ie>
2026-09-23 16:29:15 +0100

    created new book class and made an instance in main

--- src/main/java/ie/atu/oop/week1/Main.java
+++ src/main/java/ie/atu/oop/week1/Main.java
@@ -7,5 +7,16 @@ public class Main {
     {
         System.out.println("Hello World");
 
+        Book firstBook = new Book();
+        firstBook.title = "Dune";
+        firstBook.author = "Frank Herbert";
+        firstBook.pageCount = 412;
+        firstBook.available = true;
+
+        System.out.println("the title of this book is " + firstBook.title);
+        System.out.println("the books author is " + firstBook.author);
+        System.out.println("the books page count is " + firstBook.pageCount);
+        System.out.println("the status of this book is " + firstBook.available);
+
     }
 }
